a8e07672fb6e8144b5cd448fc26dd7d890646d1b
Daniel <Daniel.halaas@gmail.com>
2025-02-13 20:52:27 +0100

    Function to confirm passwords

--- script.js
+++ script.js
@@ -3,9 +3,21 @@ const signInButton = document.getElementById('logIn');
 const container = document.getElementById('container');
 
 signUpButton.addEventListener('click', () => 
-  container.classList.add('right-panel-active')
+    container.classList.add('right-panel-active')
 );
 
 signInButton.addEventListener('click', () => 
-  container.classList.remove('right-panel-active')
+    container.classList.remove('right-panel-active')
 );
+
+
+function onChange() {
+    const password = document.querySelector('input[name=password]');
+    const confirm = document.querySelector('input[name=confirm]');
+    
+    if (confirm.value == password.value) {
+        confirm.setCustomValidity('');
+    } else {
+        confirm.setCustomValidity('Passwords do not match');
+    }
+}
