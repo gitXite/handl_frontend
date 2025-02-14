b86bca0385c90c57d5bc724158e74fd809bccad9
Daniel <Daniel.halaas@gmail.com>
2025-02-14 09:33:14 +0100

    Create Footer.js

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import react from 'react';
+import './Footer.css';
+
+
+function Footer() {
+    return (
+        <footer>
+            <small>Copyright © {new Date().getFullYear()} by Handl. All Rights Reserved.</small>
+        </footer>
+    );
+}
+
+
+export default Footer;
