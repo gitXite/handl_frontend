c0668a641ecbeb525c0fb3debee2c3d821911928
gitXite <daniel.halaas@gmail.com>
2025-02-14 18:59:07 +0100

    Imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './components/LoginSignup/LoginSignup'
 import LoginSignup from './components/LoginSignup/LoginSignup';
 import Footer from './components/Footer/Footer';
