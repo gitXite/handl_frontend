5e878d1f5d632d5a18b702224a8ba03d6d650756
gitXite <daniel.halaas@gmail.com>
2025-02-14 18:57:26 +0100

    Installed React Router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './components/LoginSignup/LoginSignup'
 import LoginSignup from './components/LoginSignup/LoginSignup';
 import Footer from './components/Footer/Footer';
