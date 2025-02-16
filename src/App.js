5e279e5b03d4803b27dfe00f68500d57287d61ea
gitXite <daniel.halaas@gmail.com>
2025-02-16 14:32:49 +0100

    Spacing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import LoginSignup from './components/LoginSignup/LoginSignup';
 import Footer from './components/Footer/Footer';
 
