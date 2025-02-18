4c63bac18db240c3bb4d9b3938694da1810e1c3b
Daniel <Daniel.halaas@gmail.com>
2025-02-18 09:34:45 +0100

    Conditional rendering for login/logout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Header.css';
 import logoutImage from '../../assets/icons/logout_16dp_000000_FILL0_wght400_GRAD0_opsz20.png'
 
 
 function Header() {
-    const logout = async () => {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const checkAuthStatus = async () => {
+            try {
+                const response = await fetch('http://localhost:5000/auth/session'),  {
+                    method: 'GET', 
+                    credentials: 'include',
+                });
+                const data = await response.json();
+                setIsAuthenticated(data.isAuthenticated);
+            } catch (error) {
+                console.error('Error checking session', error);
+            }
+        };
+        checkAuthStatus();
+    }, []);
+    
+    const handleLogout = async () => {
         try {
             const response = await fetch('http://localhost:5000/auth/logout', {
                 method: 'POST',
@@ -12,11 +32,13 @@ function Header() {
                 headers: { 'Content-Type': 'application/json' },
             });
 
-            if(!response.ok) {
-                throw new Error('Logout failed');
+            if (response.ok) {
+                setIsAuthenticated(false);
+                console.log('Logged out successfully');
+                navigate('/login');
+            } else {
+                console.error('Logout failed');
             }
-
-            console.log('Logged out successfully', await response.json());
         } catch (error) {
             console.error('Failed to log out:', error.message);
         }
@@ -31,10 +53,16 @@ function Header() {
                 <a href='/'>Home</a>
                 <a href='/'>Placeholder</a>
             </div>
-            <div className='logout'>
-                <button className='logout-button' onClick={logout}>
-                    <img src={logoutImage} alt='Logout' />
-                </button>
+            <div className='auth-button'>
+                {isAuthenticated ? (
+                    <button className='logout-button' onClick={handleLogout}>
+                        <img src={logoutImage} alt='Logout' />
+                    </button>
+                ) : (
+                    <button className='login-button' onClick={() => navigate('/login')}>
+                        Login
+                    </button>
+                )}
             </div>
         </header>
     );
