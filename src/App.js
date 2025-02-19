61d98c9b2e6ae24f6ca9daa91f2fa929f344c406
gitXite <daniel.halaas@gmail.com>
2025-02-19 10:19:00 +0100

    Fixed routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Header from './components/Header/Header';
+import LandingPage from './components/LandingPage/LandingPage';
 import LoginSignup from './components/LoginSignup/LoginSignup';
 import Footer from './components/Footer/Footer';
 
@@ -10,7 +11,9 @@ function App() {
         <Router>
             <Header />
             <Routes>
+                <Route path='/' element={<LandingPage />} />
                 <Route path='/login' element={<LoginSignup />}/>
+                <Route path='/register' element={<LoginSignup />}/>
             </Routes>
             <Footer />
         </Router>
