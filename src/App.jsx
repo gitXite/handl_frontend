9a3f6af113bf91c084f370e2311ab1c642c7fea3
Daniel <Daniel.halaas@gmail.com>
2025-02-25 08:02:53 +0100

    Fixed non-unique key value. Refactored animation and conditionally animate.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import React, { useState, useRef, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AnimatePresence, motion } from 'framer-motion';
+
 import Header from '@components/Header/Header';
 import LandingPage from '@components/LandingPage/LandingPage';
 import LoginSignup from '@components/LoginSignup/LoginSignup';
@@ -13,93 +14,48 @@ import Lists from '@components/Lists/Lists';
 
 function App() {
     const [formResetTrigger, setFormResetTrigger] = useState(false);
+    const [isInitialLoad, setIsInitialLoad] = useState(true);
     const queryClient = new QueryClient();
+    const location = useLocation();
+    const prevLocationRef = useRef(null);
 
     const resetForm = () => {
         setFormResetTrigger(prev => !prev);
     };
 
+    useEffect (() => {
+        if (prevLocationRef.current !== location.pathname) {
+            setIsInitialLoad(false);
+        }
+        prevLocationRef.current = location.pathname;
+    }, [location]);
+    
+    const isLoginOrSignup = location.pathname === '/login' || location.pathname === '/register';
+
     return (
         <QueryClientProvider client={queryClient}>
             <Router>
+                <Header resetForm={resetForm} />
                 <AnimatePresence mode='sync'>
-                    <Header resetForm={resetForm} />
-                    <Routes>
-                        <Route path='/' element={
-                            <motion.div
-                                className='page-fade'
-                                key='landing'
-                                initial={{ opacity: 0 }}
-                                animate={{ opacity: 1 }}
-                                exit={{ opacity: 0 }}
-                                transition={{ duration: 0.5 }}
-                            >
-                                <LandingPage />
-                            </motion.div>
-                        } />
-                        <Route path='/login' element={
-                            <motion.div
-                                className='page-fade'
-                                key='login'
-                                initial={{ opacity: 0 }}
-                                animate={{ opacity: 1 }}
-                                exit={{ opacity: 0 }}
-                                transition={{ duration: 0.5 }}
-                            >
-                                <LoginSignup isSignUp={false} formResetTrigger={formResetTrigger} />
-                            </motion.div>
-                        } />
-                        <Route path='/register' element={
-                            <motion.div
-                                className='page-fade'
-                                key='login'
-                                initial={{ opacity: 0 }}
-                                animate={{ opacity: 1 }}
-                                exit={{ opacity: 0 }}
-                                transition={{ duration: 0.5 }}
-                            >
-                                <LoginSignup isSignUp={true} formResetTrigger={formResetTrigger} />
-                            </motion.div>
-                        } />
-                        <Route path='/lists' element={
-                            <motion.div
-                                className='page-fade'
-                                key='lists'
-                                initial={{ opacity: 0 }}
-                                animate={{ opacity: 1 }}
-                                exit={{ opacity: 0 }}
-                                transition={{ duration: 0.5 }}
-                            >
-                                <Lists />
-                            </motion.div>
-                        } />
-                        <Route path='/about' element={
-                            <motion.div
-                                className='page-fade'
-                                key='about'
-                                initial={{ opacity: 0 }}
-                                animate={{ opacity: 1 }}
-                                exit={{ opacity: 0 }}
-                                transition={{ duration: 0.5 }}
-                            >
-                                <About />
-                            </motion.div>
-                        } />
-                        <Route path='/contact' element={
-                            <motion.div
-                                className='page-fade'
-                                key='contact'
-                                initial={{ opacity: 0 }}
-                                animate={{ opacity: 1 }}
-                                exit={{ opacity: 0 }}
-                                transition={{ duration: 0.5 }}
-                            >
-                                <Contact />
-                            </motion.div>
-                        } />
-                    </Routes>
-                    <Footer />
+                    <motion.div
+                        className='page-fade'
+                        key={location.pathname}
+                        initial={{ opacity: isLoginOrSignup && isInitialLoad ? 0 : 1 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: isLoginOrSignup && isInitialLoad ? 0 : 1 }}
+                        transition={{ duration: 0.5 }}
+                    >
+                        <Routes location={location} key={location.pathname}>
+                            <Route path='/' element={<LandingPage />} />
+                            <Route path='/login' element={<LoginSignup isSignUp={false} formResetTrigger={formResetTrigger} />} />
+                            <Route path='/register' element={<LoginSignup isSignUp={true} formResetTrigger={formResetTrigger} />} />
+                            <Route path='/lists' element={<Lists />} />
+                            <Route path='/about' element={<About />} />
+                            <Route path='/contact' element={<Contact />} />
+                        </Routes>
+                    </motion.div>
                 </AnimatePresence>
+                <Footer />
             </Router>
         </QueryClientProvider>
     );
