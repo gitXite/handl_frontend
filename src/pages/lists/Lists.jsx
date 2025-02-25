fe6f741f9ee3f8963772d8effc6c6b8cc9e2235f
Daniel <Daniel.halaas@gmail.com>
2025-02-25 12:55:43 +0100

    Update Lists.jsx

--- src/pages/lists/Lists.jsx
+++ src/pages/lists/Lists.jsx
@@ -1,8 +1,44 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 import './Lists.css';
 
 
 function Lists() {
+    const navigate = useNavigate();
+
+    // API call to fetch session
+    const fetchSession = async () => {
+        try {
+            const { data } = await axios.get('http://localhost:5000/api/auth/get-session', {
+                withCredentials: true,
+            });
+
+            console.log('Parsed JSON:', data);
+            return data;
+        } catch (error) {
+            if (axios.isAxiosError(error)) {
+                console.error('Axios error:', error.response?.data || error.message);
+                throw new Error(error.response?.data?.message || 'Failed to fetch authentication status');
+            }
+            throw error;
+        }
+    };
+
+    // Fetch session
+    const { data, isError } = useQuery({
+        queryKey: ['session'],
+        queryFn: fetchSession,
+        retry: false
+    });
+
+    // If fetchSession fails or data returns false, redirects to /login and returns
+    if (isError || !data?.isAuthenticated) {
+        navigate('/login');
+        return <p>Log in to access</p>;
+    }
+    
     return (
         <div className='list-container'>
             <h1>Your shopping lists</h1>
