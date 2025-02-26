97e995e8668e05dac7dce7521671acaba805f639
gitXite <daniel.halaas@gmail.com>
2025-02-26 13:15:24 +0100

    @media and animation work

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { motion } from 'framer-motion';
+import { delay, motion } from 'framer-motion';
 
 import github from '@assets/icons/github_2504911.png';
 import instagram from '@assets/icons/instagram_2504918.png';
@@ -7,57 +7,67 @@ import linkedin from '@assets/icons/linkedin_2504923.png';
 import './Contact.css';
 
 
+const MotionWrapper = ({ className, children, transition = {} }) => {
+    return <motion.div
+        className={className}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.5, ...transition }}
+        >
+            {children}
+        </motion.div>
+};
+
 function Contact() {
     return (
         <div className='contact-container'>
             <div className='contact-left'>
                 <h1>Contact Us</h1>
-                <motion.div
-                    className='fade-contact'
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0 }}
-                    transition={{ duration: 0.5, delay: 0 }}
-                >
+                <MotionWrapper className={'fade-contact'} transition={{ delay: 0 }}>
                     <p>Feel free to reach out</p>
-                </motion.div>
-                <motion.div
-                    className='fade-contact'
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0 }}
-                    transition={{ duration: 0.5, delay: 0.2 }}
-                >
+                </MotionWrapper>
+                <MotionWrapper className={'fade-contact'} transition={{ delay: 0.2 }}>
                     <p>to one of our developers,</p>
-                </motion.div>
-                <motion.div
-                    className='fade-contact'
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0 }}
-                    transition={{ duration: 0.5, delay: 0.4 }}
-                >
+                </MotionWrapper>
+                <MotionWrapper className={'fade-contact'} transition={{ delay: 0.4 }}>
                     <p>we want to help</p>
-                </motion.div>
+                </MotionWrapper>
                 <div className='socials'>
-                    <a href='https://www.github.com/gitXite' target='_blank' rel='noopener noreferrer'>
-                        <img src={github}></img>
-                    </a>
-                    <a href='https://www.instagram.com/daniel_halaas' target='_blank' rel='noopener noreferrer'>
-                        <img src={instagram}></img>
-                    </a>
-                    <a href='https://www.linkedin.com/in/daniel-halås-b00363352' target='_blank' rel='noopener noreferrer'>
-                        <img src={linkedin}></img>
-                    </a>
+                    <MotionWrapper className={'fade-contact'} transition={{ delay: 0.4 }}>
+                        <a href='https://www.github.com/gitXite' target='_blank' rel='noopener noreferrer'>
+                            <img src={github}></img>
+                        </a>
+                    </MotionWrapper>
+                    <MotionWrapper className={'fade-contact'} transition={{ delay: 0.5 }}>
+                        <a href='https://www.instagram.com/daniel_halaas' target='_blank' rel='noopener noreferrer'>
+                            <img src={instagram}></img>
+                        </a>
+                    </MotionWrapper>
+                    <MotionWrapper className={'fade-contact'} transition={{ delay: 0.6 }}>
+                        <a href='https://www.linkedin.com/in/daniel-halås-b00363352' target='_blank' rel='noopener noreferrer'>
+                            <img src={linkedin}></img>
+                        </a>
+                    </MotionWrapper>
                 </div>
             </div>
             <div className='contact-right'>
                 <form className='contact-form'>
-                    <input type='text' placeholder='Name' required/>
-                    <input type='email' placeholder='Email' required/>
-                    <input type='text' placeholder='Subject' required/>
-                    <input type='text' placeholder='Message' required/>
-                    <button type='submit'>Submit</button>
+                    <MotionWrapper className={'fade-form'} transition={{ delay: 0.2 }}>
+                        <input type='text' placeholder='Name' required/>
+                    </MotionWrapper>
+                    <MotionWrapper className={'fade-form'} transition={{ delay: 0.3 }}>
+                        <input type='email' placeholder='Email' required/>
+                    </MotionWrapper>
+                    <MotionWrapper className={'fade-form'} transition={{ delay: 0.4}}>
+                        <input type='text' placeholder='Subject' required/>
+                    </MotionWrapper>
+                    <MotionWrapper className={'fade-form'} transition={{ delay: 0.5 }}>
+                        <input type='text' placeholder='Message' required/>
+                    </MotionWrapper>
+                    <MotionWrapper className={'fade-form'} transition={{ delay: 0.6 }}>
+                        <button type='submit'>Submit</button>
+                    </MotionWrapper>
                 </form>
             </div>
         </div>
