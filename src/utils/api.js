8bbd78f62e20eab1ab9b750982f7dfac50cee0a3
Daniel <Daniel.halaas@gmail.com>
2025-02-27 17:45:15 +0100

    Generic methods for HTTP requests

---
+++ src/utils/api.js
@@ -0,0 +1,40 @@
+import axios from '@axiosConfig';
+
+
+const api = {
+    get: async (endpoint, config = {}) => {
+        try {
+            const response = await axios.get(endpoint, config);
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    },
+    post: async (endpoint, data, config = {}) => {
+        try {
+            const response = await axios.post(endpoint, data, config);
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    },
+    put: async (endpoint, data, config = {}) => {
+        try {
+            const response = await axios.put(endpoint, data, config);
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    },
+    delete: async (endpoint, config = {}) => {
+        try {
+            const response = await axios.delete(endpoint, config);
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    },
+};
+
+
+export default api;
