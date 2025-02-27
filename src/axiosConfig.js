98a9110f694b3d274015e23155332950d480e692
Daniel <Daniel.halaas@gmail.com>
2025-02-27 17:03:45 +0100

    Update axiosConfig.js

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -3,4 +3,6 @@ import axios from 'axios';
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
+axios.defaults.headers['Content-Type'] = 'application/json';
+
 export default axios;
