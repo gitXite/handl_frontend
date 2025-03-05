4ac642cc273ccaedd51087704d3f554ebb1344cd
Daniel <Daniel.halaas@gmail.com>
2025-03-05 16:31:50 +0100

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,8 +5,17 @@ import './Footer.css';
 function Footer() {
     return (
         <footer>
-            <div className='footer'>
+            <div className='footer-logo'>
+                <h1>HANDL</h1>
+            </div>
+            <div className='footer-copyright'>
                 <small>Copyright © {new Date().getFullYear()} by HANDL. All Rights Reserved.</small>
+            </div>
+            <div className='footer-socials'>
+                <img />
+                <img />
+                <img />
+                <small>support@handl.dev</small>
             </div>
         </footer>
     );
