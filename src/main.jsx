cfe030fdf9be5508815bc0ffcbabdfa4d48d25a2
gitXite <daniel.halaas@gmail.com>
2025-03-06 12:20:48 +0100

    Added light and darkmode favicons

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+
+function updateManifest() {
+    const isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    const manifest = document.querySelector('link[rel="manifest"]');
+    if (manifest) {
+        manifest.href = isDarkMode ? "/dark-site.webmanifest" : "/light-site.webmanifest";
+    }
+}
+
+updateManifest();
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", updateManifest);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
