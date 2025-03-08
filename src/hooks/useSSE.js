90901f8815fb4eb5a7291b5ad261547c45b70c18
Daniel <Daniel.halaas@gmail.com>
2025-03-08 13:07:16 +0100

    Finished useSSE hook with item fetch for listId

--- src/hooks/useSSE.js
+++ src/hooks/useSSE.js
@@ -2,6 +2,33 @@ import { useEffect, useState } from 'react';
 import api from '../utils/api';
 
 
-const useSSE = () => {
+const useSSE = (listId) => {
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        api.get(`/api/lists/${listId}/items`)
+            .then(res => setItems(res.data))
+            .catch(err => console.error('Error fetching items:', err));
+
+        const eventSource = new EventSource(`${import.meta.env.VITE_API_URL}/events?${listId}`);
+
+        eventSource.onmessage = (event) => {
+            const { event: type, data } = JSON.parse(event.data);
+
+            if (type === 'ITEM_ADDED') {
+                setItems(prev => [...prev, data]);
+            } else if (type === 'ITEM_UPDATED') {
+                setItems(prev => prev.map(item => (item.id === data.id ? data : item)));
+            } else if (type === 'ITEM_DELETED') {
+                setItems(prev => prev.filter(item => item.id !== data.id));
+            }
+        };
+
+        return () => eventSource.close();
+    }, [listId])
     
+    return items;
 };
+
+
+export default useSSE;
