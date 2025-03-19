260c9065f41d95dc1123a5dc62611f000d6281fd
Daniel <Daniel.halaas@gmail.com>
2025-03-19 09:41:39 +0100

    Update handleFunctions.js

--- src/utils/handleFunctions.js
+++ src/utils/handleFunctions.js
@@ -6,7 +6,7 @@ export const handleChange = (e, formData, setFormData, setPasswordErrors) => {
     const { name, value } = e.target;
     setFormData({...formData, [name]: value});
 
-    if (name === 'password') {
+    if (setPasswordErrors && name === 'password') {
         const validation = validatePassword(value);
         setPasswordErrors(validation.errors);
     }
