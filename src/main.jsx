0eedf734892a53441209ea2c34b4652cd3adafa7
gitXite <daniel.halaas@gmail.com>
2025-03-19 16:00:33 +0100

    Removed dark mode manifest update

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,16 @@ import App from './App';
 import './index.css';
 
 
-function updateManifest() {
-    const isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
-    const manifest = document.querySelector('link[rel="manifest"]');
-    if (manifest) {
-        manifest.href = isDarkMode ? "/dark-site.webmanifest" : "/light-site.webmanifest";
-    }
-}
+// function updateManifest() {
+//     const isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+//     const manifest = document.querySelector('link[rel="manifest"]');
+//     if (manifest) {
+//         manifest.href = isDarkMode ? "/dark-site.webmanifest" : "/light-site.webmanifest";
+//     }
+// }
 
-updateManifest();
-window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", updateManifest);
+// updateManifest();
+// window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", updateManifest);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
