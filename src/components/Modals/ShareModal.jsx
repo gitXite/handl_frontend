54d5d87fa1b6bdd9f4166c8131422204fe6cf2d4
Daniel <Daniel.halaas@gmail.com>
2025-03-19 13:56:30 +0100

    Create ShareModal.jsx

---
+++ src/components/Modals/ShareModal.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import MotionWrapper from '@components/MotionWrapper';
+import { useHotkeys } from 'react-hotkeys-hook';
+
+import { handleChange } from '@utils/handleFunctions';
+
+import './ShareModal.css';
+
+
+function ShareModal({ message, onCancel, onConfirm }) {
+    const [formData, setFormData] = useState({
+        email: ''
+    });
+    
+    const handleConfirm = (e) => {
+        e.preventDefault();
+        
+        const email = formData.email;
+        onConfirm(email);
+    };
+
+    useHotkeys('enter', handleConfirm);
+    useHotkeys('escape', onCancel);
+
+    return (
+        <MotionWrapper className={'modal-overlay'}>
+            <div className='modal'>
+                <p>{message}</p>
+                <form className='modal-form'>
+                    <div className='field'>
+                        <input
+                            name='email'
+                            type='email'
+                            value={formData.email}
+                            onChange={(e) => handleChange(e, formData, setFormData)}
+                            required
+                        />
+                        <label className='form-label' htmlFor='email'>Email</label>
+                    </div>
+                </form>
+                <div className='modal-actions'>
+                    <button className='share' onClick={handleConfirm}>Share</button>
+                    <button onClick={onCancel}>Cancel</button>
+                </div>
+            </div>
+        </MotionWrapper>
+    );
+}
+
+
+export default ShareModal;
