94acde16ba650c9699c469737d3e79edef6c268a
Daniel <Daniel.halaas@gmail.com>
2025-03-22 15:23:35 +0100

    Update ItemsPage.jsx

--- src/pages/ListPage/ItemsPage/ItemsPage.jsx
+++ src/pages/ListPage/ItemsPage/ItemsPage.jsx
@@ -1,8 +1,11 @@
-
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import './Items.css';
 
 
 function ItemsPage() {
+    const { id } = useParams();
+    
     return (
         <div className='items-container'>
             <div className='items-subcontainer'>
