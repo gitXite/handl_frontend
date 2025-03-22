2a37c2945a94f064d52da8a1521c111478505202
Daniel <Daniel.halaas@gmail.com>
2025-03-22 15:03:44 +0100

    Boilerplate

--- src/pages/ListPage/ItemsPage/ItemsPage.jsx
+++ src/pages/ListPage/ItemsPage/ItemsPage.jsx
@@ -1,1 +1,13 @@
 
+import './Items.css';
+
+
+function ItemsPage() {
+    return (
+        <div className='items-container'>
+            <div className='items-subcontainer'>
+                
+            </div>
+        </div>
+    );
+}
