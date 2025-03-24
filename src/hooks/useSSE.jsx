05115be88a3590c823bce3bbead787894b738b02
gitXite <daniel.halaas@gmail.com>
2025-03-24 11:48:37 +0100

    Made new useSSE hook

--- src/hooks/useSSE.jsx
+++ src/hooks/useSSE.jsx
@@ -2,32 +2,27 @@ import { useEffect, useState } from 'react';
 
 
 const useSSE = (listId) => {
-    const [items, setItems] = useState([]);
-    const [lists, setLists] = useState([]);
+    const [data, setData] = useState(null);
 
     useEffect(() => {
-        const eventSource = new EventSource(`${import.meta.env.VITE_API_URL}/events?${listId}`);
+        const eventSource = new EventSource(`${import.meta.env.VITE_API_URL}/stream`);
 
-        eventSource.onmessage = (event) => {
-            const { event: type, data } = JSON.parse(event.data);
-
-            if (type === 'ITEM_ADDED') {
-                setItems(prev => [...prev, data]);
-            } else if (type === 'ITEM_UPDATED') {
-                setItems(prev => prev.map(item => (item.id === data.id ? data : item)));
-            } else if (type === 'ITEM_DELETED') {
-                setItems(prev => prev.filter(item => item.id !== data.id));
-            } else if (type === 'LIST_ADDED') {
-                setLists(prev => [...prev, data]);
-            } else if (type === 'LIST_DELETED') {
-                setLists(prev => prev.filter(list => list.id !== data.id));
-            }
+        eventSource.onmessage = function(event) {
+            const receivedData = JSON.parse(event.data);
+            setData(receivedData);
         };
 
-        return () => eventSource.close();
-    }, [listId])
+        eventSource.onerror = function() {
+            console.error('Error with SSE connection');
+            eventSource.close();
+        };
+
+        return () => {
+            eventSource.close();
+        };
+    }, []);
     
-    return { items, lists };
+    return data;
 };
 
 
