9658de336ca3b7ef0c2bbd8edd04c5c1650e7f70
Daniel <Daniel.halaas@gmail.com>
2025-03-25 10:08:00 +0100

    Update AuthContext.jsx to get currentUser object

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -10,17 +10,27 @@ export const AuthProvider = ({ children }) => {
         return storedAuth ? JSON.parse(storedAuth) : false;
     });
 
+    const [currentUser, setCurrentUser] = useState(() => {
+        const storedUser = localStorage.getItem('currentUser');
+        return storedUser ? JSON.parse(storedUser) : null;
+    });
+
     useEffect(() => {
         const checkSession = async () => {
             try {
                 const result = await api.get('/api/auth/session');
                 const isAuthenticated = result.isAuthenticated || false;
+                const user = result.user || null;
                 setIsAuthenticated(isAuthenticated);
+                setCurrentUser(user);
                 localStorage.setItem('isAuthenticated', JSON.stringify(isAuthenticated));
+                localStorage.setItem(currentUser', JSON.stringify(user));
             } catch (error) {
                 console.error('Error fetching session:', error.message);
                 setIsAuthenticated(false);
+                setCurrentUser(null);
                 localStorage.removeItem('isAuthenticated');
+                localStorage.removeItem('currentUser');
             }
         };
 
@@ -30,6 +40,8 @@ export const AuthProvider = ({ children }) => {
     const value = {
         isAuthenticated,
         setIsAuthenticated,
+        currentUser,
+        setCurrentUser,
     };
 
     return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
