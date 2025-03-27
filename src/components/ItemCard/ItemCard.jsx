e47c7b3feaccfa3dce60b8fd71f6c1ea495c79b1
Daniel <Daniel.halaas@gmail.com>
2025-03-27 20:54:17 +0100

    Update ItemCard.jsx

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { Tooltip, Zoom } from '@mui/material';
 import MotionWrapper from '../MotionWrapper';
 import './ItemCard.css';
 
@@ -10,17 +11,8 @@ function ItemCard({ item, onModal }) {
         setIsChecked(item.checked);
     }, [item.checked]);
     
-    const handleCheckItem = async (listId, itemId) => {
-        setIsChecked(true);
-        try {
-            await api.patch(`/api/lists/${listId}/items/${itemId}/check`, { isChecked });
-        } catch (error) {
-            console.error('Failed to set check status:', error);
-        }
-    };
-
-    const handleUncheckItem = async (listId, itemId) => {
-        setIsChecked(false);
+    const handleCheckItem = async (listId, itemId, bool) => {
+        setIsChecked(bool);
         try {
             await api.patch(`/api/lists/${listId}/items/${itemId}/check`, { isChecked });
         } catch (error) {
@@ -30,8 +22,32 @@ function ItemCard({ item, onModal }) {
     
     return (
         <div className='item-card'>
-            <MotionWrapper className={'item-fade'}>
-
+            <MotionWrapper className={'item-fade'} transition={{ delay: 0.2 }}>
+                <Tooltip
+                    title='Edit item'
+                    disableInteractive
+                    slots={{
+                        transition: Zoom,
+                    }}
+                    enterDelay={500}
+                    enterNextDelay={500}
+                    slotProps={{
+                        popper: {
+                            modifiers: [
+                                {
+                                    name: 'offset',
+                                    options: { offset: [0, -6] },
+                                },
+                            ],
+                        },
+                    }}
+                >
+                    <button className='item' onClick={() => onModal('edit', item.id)}>
+                        <div className='item-name'>
+                            <p>{item.name}</p>
+                        </div>
+                    </button>
+                </Tooltip>
             </MotionWrapper>
         </div>
     );
