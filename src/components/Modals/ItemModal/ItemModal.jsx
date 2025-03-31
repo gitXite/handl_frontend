50b8760e8c371da5269e2ac3abbb7a180207a74c
gitXite <daniel.halaas@gmail.com>
2025-03-31 11:04:59 +0200

    Item modal work

--- src/components/Modals/ItemModal/ItemModal.jsx
+++ src/components/Modals/ItemModal/ItemModal.jsx
@@ -14,25 +14,36 @@ function ItemModal({ message, onConfirm, onCancel }) {
 
     const handleChange = () => {};
     
-    useHotkeys('enter', (e) => handleConfirm(e, formData.prop, onConfirm, setNotice));
+    useHotkeys('enter', (e) => handleConfirm(e, formData, onConfirm, setNotice));
     useHotkeys('escape', onCancel);
 
     return(
         <MotionWrapper className={'modal-overlay'} transition={{ duration: 0.2 }}>
             <div className='item-modal'>
                 <p>{message}</p>
-                <form className='modal-form' onSubmit={(e) => {e.preventDefault(); handleConfirm(e, formData.name, onConfirm, setNotice);}}>
+                <form className='modal-form' onSubmit={(e) => {e.preventDefault(); handleConfirm(e, formData, onConfirm, setNotice);}}>
                     <div className='modal-field'>
                         <input
-                            name=''
+                            name='name'
                             type='text'
-                            value={formData.prop}
+                            value={formData.name}
                             onChange={(e) => handleChange(e)}
                             onFocus={handleFocus}
                             autoFocus
                             required
                         />
-                        <label className='form-label' htmlFor=''></label>
+                        <label className='form-label' htmlFor='name'>Name</label>
+                    </div>
+                    <div className=''>
+                        <input
+                            name='quantity'
+                            type='number'
+                            value={formData.quantity}
+                            onChange={(e) => handleChange(e)}
+                            onFocus={handleFocus}
+                            required
+                        />
+                        <label className='form-label' htmlFor='quantity'></label>
                     </div>
                 </form>
                 <div className='modal-notice-container'>
@@ -43,7 +54,7 @@ function ItemModal({ message, onConfirm, onCancel }) {
                     )}
                 </div>
                 <div className='modal-actions'>
-                    <button className='confirm' type='submit' onClick={(e) => handleConfirm(e, formData.prop, onConfirm, setNotice)}>Confirm</button>
+                    <button className='confirm' type='submit' onClick={(e) => handleConfirm(e, formData, onConfirm, setNotice)}>Confirm</button>
                     <button onClick={onCancel}>Cancel</button>
                 </div>
             </div>
